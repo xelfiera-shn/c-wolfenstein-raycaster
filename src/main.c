06476740e25651ad07a6444015a742c8e51f33ef
xelfiera <ahmet.sahin9663@gmail.com>
2026-02-08 23:44:03 +0300

    Basic raycaster implementation

--- src/main.c
+++ src/main.c
@@ -42,14 +42,14 @@ int main(int argc, const char* argv[]) {
 
     Player player;
     player.Pos.x = 200.f;
-    player.Pos.y = 300.f;
+    player.Pos.y = 700.f;
     player.Angle = PI / 2.f;
 
     while (!glfwWindowShouldClose(window)) {
         clearDisplay();
+        
+        renderView(&player);
         renderMinimap(&player);
-
-        // renderView(&player);
         
         handleInput(window, &player);
 
@@ -67,7 +67,120 @@ void clearDisplay() {
 }
 
 void renderView(Player* p) {
+    int pCellX = p->Pos.x / CELL_SIZE;
+    int pCellY = p->Pos.y / CELL_SIZE;
+    float da = PLAYER_VIEW_ANGLE / DISPLAY_WIDTH;
 
+    float ra = p->Angle + PLAYER_VIEW_ANGLE / 2.0;
+
+    for (int i = 0; i < DISPLAY_WIDTH; i++) {
+        if (ra < 0) ra += 2 * PI;
+        else if (ra >= 2 * PI) ra -= 2 * PI;
+
+        int dv, dh, ov, oh;
+        if (ra < PI) {
+            dh = -1;
+            oh = -1;
+        } else {
+            dh = 1;
+            oh = CELL_SIZE;
+        }
+
+        if (ra > PI / 2 && ra < 3 * PI / 2) {
+            dv = -1;
+            ov = -1;
+        } else {
+            dv = 1;
+            ov = CELL_SIZE;
+        }
+
+        float rTan = tan(ra);
+
+        // Calculate horizontal hits
+        float distH = 100000;
+        int hx = p->Pos.x;
+        int hy = p->Pos.y;
+        if (ra != 0 && ra != PI) {
+            for (int h = 0; h < mapH; h++) {
+                hy = (pCellY + h * dh) * CELL_SIZE + oh;
+                hx = ((p->Pos.y - hy) / rTan) + p->Pos.x;
+
+                int cx = hx / CELL_SIZE;
+                int cy = hy / CELL_SIZE;
+                if (cx < 0 || cx >= mapW || cy < 0 || cy >= mapH) break;
+
+                if (map[cy * mapW + cx] > 0) {
+                    distH = sqrt((p->Pos.x - hx) * (p->Pos.x - hx) + (p->Pos.y - hy) * (p->Pos.y - hy));
+                    break;
+                }
+            }
+        }
+
+        // Calculate vertical hits
+        float distV = 100000;
+        int vx = p->Pos.x;
+        int vy = p->Pos.y;
+        if (ra != PI / 2 && ra != 3 * PI / 2) {
+            for (int v = 0; v < mapW; v++) {
+                vx = (pCellX + v * dv) * CELL_SIZE + ov;
+                vy = ((p->Pos.x - vx) * rTan) + p->Pos.y;
+
+                int cx = vx / CELL_SIZE;
+                int cy = vy / CELL_SIZE;
+                if (cx < 0 || cx >= mapW || cy < 0 || cy >= mapH) break;
+
+                if (map[cy * mapW + cx] > 0) {
+                    distV = sqrt((p->Pos.x - vx) * (p->Pos.x - vx) + (p->Pos.y - vy) * (p->Pos.y - vy));
+                    break;
+                }
+            }
+        }
+
+        // Compute valid ray
+        int rx, ry, rl;
+        if (distH < distV) {
+            rx = hx; ry = hy; rl = distH;
+        } else {
+            rx = vx; ry = vy; rl = distV;
+        }
+
+        float ca = p->Angle - ra;
+        if (ca < 0) ca += 2 * PI;
+        else if (ca >= 2 * PI) ca -= 2 * PI;
+        rl = rl * cos(ca);
+
+        if (rl < 1) rl = 1;
+        int lh = DISPLAY_HEIGHT * CELL_SIZE / rl;
+        if (lh > DISPLAY_HEIGHT) lh = DISPLAY_HEIGHT;
+
+        int sx = i;
+        int sy = (DISPLAY_HEIGHT - lh) / 2;
+
+        glColor3f(0.f, 1.f, 1.f);
+        glLineWidth(1);
+        glBegin(GL_LINES);
+        glVertex2i(sx, 0);
+        glVertex2i(sx, sy);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex2i(sx, sy);
+        glVertex2i(sx, sy + lh);
+        glEnd();
+
+        // // DEBUG BEGIN
+        // int px = DISPLAY_WIDTH + MINIMAP_WIDTH_RATIO * (p->Pos.x - DISPLAY_WIDTH);
+        // int py = DISPLAY_HEIGHT + MINIMAP_HEIGHT_RATIO * (p->Pos.y - DISPLAY_HEIGHT);
+        // int rrx = DISPLAY_WIDTH + MINIMAP_WIDTH_RATIO * (rx - DISPLAY_WIDTH);
+        // int rry = DISPLAY_HEIGHT + MINIMAP_HEIGHT_RATIO * (ry - DISPLAY_HEIGHT);
+        // glColor3f(1.f, 0.f, 0.f);
+        // glLineWidth(1);
+        // glBegin(GL_LINES);
+        // glVertex2i(px, py);
+        // glVertex2i(rrx, rry);
+        // glEnd();
+        // // DEBUG END
+
+        ra -= da;
+    }
 }
 
 void renderMinimap(Player* p) {
@@ -136,7 +249,7 @@ void handleInput(GLFWwindow* w, Player* p) {
         }
     }
 
-    else if (glfwGetKey(w, GLFW_KEY_W) == GLFW_PRESS) {
+    else if (glfwGetKey(w, GLFW_KEY_S) == GLFW_PRESS) {
         float newX = p->Pos.x - p->Delta.x;
         float newY = p->Pos.y + p->Delta.y;
 
